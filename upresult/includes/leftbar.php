2d0ea1753299c079cba1c450630a19ebc73865d5
QuangVinhDev <235371vinhtran@gmail.com>
2024-03-30 17:46:18 +0700

    upload courses

---
+++ upresult/includes/leftbar.php
@@ -0,0 +1,70 @@
+<div class="left-sidebar box-shadow 
+" style="
+    background-color: orange; ">
+                        <div class="sidebar-content">
+                            <div class="user-info">
+                                <img src="assets/images/logo.jpg" alt="Profile" class="img-circle profile-img" width="40px">
+                                <p style="color: white;">Administrator</p>
+                            </div>
+                            <!-- /.user-info -->
+
+                            <div class="sidebar-nav">
+                                <ul class="side-nav color-gray">
+                                    <li class="nav-header">
+                                        <span class="">Main Category</span>
+                                    </li>
+                                    <li>
+                                        <a href="dashboard.php"><i class="fa fa-dashboard"></i> <span>Dashboard</span> </a>
+                                     
+                                    </li>
+
+                                    <li class="nav-header">
+                                        <span class="">Appearance</span>
+                                    </li>
+                                    <li class="has-children">
+                                        <a href="#"><i class="fa fa-file-text"></i> <span>Employee Class</span> <i class="fa fa-angle-right arrow"></i></a>
+                                        <ul class="child-nav">
+                                            <li><a href="create-class.php"><i class="fa fa-plus"></i> <span>Create Class</span></a></li>
+                                            <li><a href="manage-classes.php"><i class="fa fa-bars"></i> <span>Manage Classes</span></a></li>
+                                           
+                                        </ul>
+                                    </li>
+  <li class="has-children">
+                                        <a href="#"><i class="fa fa-book"></i> <span>Course</span> <i class="fa fa-angle-right arrow"></i></a>
+                                        <ul class="child-nav">
+                                            <li><a href="create-subject.php"><i class="fa fa-plus"></i> <span>Create Course</span></a></li>
+                                            <li><a href="manage-subjects.php"><i class="fa fa-bars"></i> <span>Manage Course</span></a></li>
+                                           <li><a href="add-subjectcombination.php"><i class="fa fa-plus"></i> <span>Add Course Combination </span></a></li>
+                                           <a href="manage-subjectcombination.php"><i class="fa fa-bars"></i> <span>Manage Course Combination </span></a></li>
+                                        </ul>
+                                    </li>
+   <li class="has-children">
+                                        <a href="#"><i class="fa fa-users"></i> <span>Employees</span> <i class="fa fa-angle-right arrow"></i></a>
+                                        <ul class="child-nav">
+                                            <li><a href="add-students.php"><i class="fa fa-plus"></i> <span>Add Employees</span></a></li>
+                                            <li><a href="manage-students.php"><i class="fa fa-bars"></i> <span>Manage Employees</span></a></li>
+                                           
+                                        </ul>
+                                    </li>     </li>
+
+
+<li class="has-children">
+                                        <a href="#"><i class="fa fa-bell"></i> <span>Notices</span> <i class="fa fa-angle-right arrow"></i></a>
+                                        <ul class="child-nav">
+                                            <li><a href="add-notice.php"><i class="fa fa-plus"></i> <span>Add Notice</span></a></li>
+                                            <li><a href="manage-notices.php"><i class="fa fa-bars"></i> <span>Manage Notices</span></a></li>
+                                           
+                                        </ul>        </li>
+
+
+
+                                        <li><a href="change-password.php"><i class="fa fa-key"></i> <span> Change Password</span></a></li>
+
+
+                                           
+                            
+                            </div>
+                            <!-- /.sidebar-nav -->
+                        </div>
+                        <!-- /.sidebar-content -->
+                    </div>
---
+++ upresult/includes/topbar.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>UP-RESULT Software by Mayuri K.| Dashboard</title>
+        <link rel="icon" type="image/x-icon" href="assets/images/favicon.png">
+
+        <link rel="stylesheet" href="assets/css/bootstrap.min.css" media="screen" >
+        <link rel="stylesheet" href="assets/css/font-awesome.min.css" media="screen" >
+        <link rel="stylesheet" href="assets/css/animate-css/animate.min.css" media="screen" >
+        <link rel="stylesheet" href="assets/css/lobipanel/lobipanel.min.css" media="screen" >
+        <link rel="stylesheet" href="assets/css/toastr/toastr.min.css" media="screen" >
+        <link rel="stylesheet" href="assets/css/icheck/skins/line/blue.css" >
+        <link rel="stylesheet" href="assets/css/icheck/skins/line/red.css" >
+        <link rel="stylesheet" href="assets/css/icheck/skins/line/green.css" >
+        <link rel="stylesheet" href="assets/css/main.css" media="screen" >
+        <link rel="stylesheet" href="assets/css/prism/prism.css" media="screen" >
+        
+        <!-- <link rel="stylesheet" type="text/css" href="assets/js/DataTables/datatables.min.css"/> -->
+        <script src="assets/js/modernizr/modernizr.min.js"></script>
+         <style>
+        .errorWrap {
+    padding: 10px;
+    margin: 0 0 20px 0;
+    background: #fff;
+    border-left: 4px solid #dd3d36;
+    -webkit-box-shadow: 0 1px 1px 0 rgba(0,0,0,.1);
+    box-shadow: 0 1px 1px 0 rgba(0,0,0,.1);
+}
+.succWrap{
+    padding: 10px;
+    margin: 0 0 20px 0;
+    background: #fff;
+    border-left: 4px solid #5cb85c;
+    -webkit-box-shadow: 0 1px 1px 0 rgba(0,0,0,.1);
+    box-shadow: 0 1px 1px 0 rgba(0,0,0,.1);
+}
+        </style>
+    </head>
+     <div id="page"></div>
+        <div id="loading"></div>
+    <body class="top-navbar-fixed">
+
+        <div class="main-wrapper">
+  <nav class="navbar top-navbar bg-white" style="background-color: orange;">
+            	<div class="container-fluid">
+                    <div class="row">
+                        <div class="navbar-header no-padding">
+
+                		</div>
+                        <!-- /.navbar-header -->
+
+                		<div class="collapse navbar-collapse" id="navbar-collapse-1">
+                            <!-- /.nav navbar-nav -->
+     
+                			<ul class="nav navbar-nav navbar-right" data-dropdown-in="fadeIn" data-dropdown-out="fadeOut">
+                				
+                				    <li>
+                                    <a href="logout.php" class="color-danger text-center"><i class="fa fa-sign-out"></i> Logout</a></li>
+                					
+                		
+                            
+                			</ul>
+                            <!-- /.nav navbar-nav navbar-right -->
+                		</div>
+                		<!-- /.navbar-collapse -->
+                    </div>
+                    <!-- /.row -->
+            	</div>
+            	<!-- /.container-fluid -->
+            </nav>
